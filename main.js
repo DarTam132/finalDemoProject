91e4b3958c021bd6ac34732472780ffb76b1c300
DarTam132 <darius.tamasan96@gmail.com>
2023-10-15 14:27:34 +0300

    further action

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML = `
+document.querySelector("#app").innerHTML += `
   <div>
  <h2>Darius Tamasan</h2>
+ <i class="fa-regular fa-heart">hej</i>
   </div>
 `;
-
-setupCounter(document.querySelector("#counter"));
